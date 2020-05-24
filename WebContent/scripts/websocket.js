ea7d10d5dc8403291f928006fa4e0c273bc58433
Andy Cheung <micro_zx@msn.cn>
2020-05-24 19:46:37 +0800

    2020.5.24 用户名重名体验优化。

--- WebContent/scripts/websocket.js
+++ WebContent/scripts/websocket.js
@@ -18,23 +18,36 @@
 let ws;
 let nickname;
 let uuid;
+let reinputDialogHandle;
+let reinputNN = false;
 
 let initWS = (host, port, nicknameToSend) => {
     ws = new WebSocket("ws://" + host + ":" + port + "/acmcs/wshandler");
 
     ws.onopen = function () {
+        // Hide window if the user is re-inputting username.
+        if (reinputNN) {
+            $(reinputDialogHandle).modal("hide");
+            reinputNN = false;
+        }
+        
         ws.send(CHECK_DUPLICATE_REQUEST_HEADER + nicknameToSend);
     };
 
     ws.onmessage = function (message){
         if (message.data.indexOf(WEBSOCKET_UUID_HEADER) != -1) {
             uuid = message.data.replace(WEBSOCKET_UUID_HEADER, "").replace(WEBSOCKET_UUID_TAIL, "");
-            document.getElementById("chatlog").textContent += "Connected to Server. UUID: " + uuid + "  Nickname: " + nicknameToSend + "\n";
+            document.getElementById("chatlog").textContent += "Handshake to Server Successfully. UUID: " + uuid + "  Nickname: " + nicknameToSend + "\n";
             return;
         } else if (message.data.indexOf(USER_NAME_DUPLICATED) != -1) {
-            alert("用户名重复了！请刷新页面重试！");
             ws.close();
-        } else if (message.data.indexOf(USER_NAME_NOT_EXIST) != -1) {
+            reinputDialogHandle = inputBox("提示", "用户名重复了，请重新输入昵称：", (name) => {
+                reinputNN = true;
+                initWS(host, port, name);
+                
+                return true;
+            });
+        } else if (message.data.indexOf(USER_NAME_NOT_EXIST) != -1) {  
             // Formally register.
             ws.send(CONNECTING_GREET_LEFT_HALF + uuid + CONNECTING_GREET_MIDDLE_HALF + nicknameToSend + CONNECTING_GREET_RIGHT_HALF);
             nickname = nicknameToSend;
@@ -51,8 +64,8 @@ let initWS = (host, port, nicknameToSend) => {
 let send = () => {
     let msg = document.getElementById("msg").value;
     ws.send(MESSAGE_HEADER_LEFT_HALF + uuid + MESSAGE_HEADER_MIDDLE_HALF + MESSAGE_HEADER_RIGHT_HALF + msg);
-    document.getElementById("chatlog").textContent += nickname + ": " + msg + "\n";
-    document.getElementById("msg").value = "";
+    $("#chatlog")[0].textContent += nickname + ": " + msg + "\n";
+    $("#msg")[0].value = "";
 };
 
 let closeConnection = () => {
